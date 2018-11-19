1e7b58359bbd48ecac9c1eb39f9741506ae72ca7
onnsui <ryo.sato@scouter.co,jp>
2018-11-20 01:39:19 +0900

    footer作成

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,8 +5,8 @@
 </template>
 
 <script>
-import Header from '~/components/Header.vue'
-import Footer from '~/components/Footer.vue'
+import Header from '~/components/item/Header.vue'
+import Footer from '~/components/item/Footer.vue'
 
 export default {
   components: {
